89bdb94e27d98fe0c310cd514acbb202bb258732
Brindia Joseph <brindiatyjuana@gmail.com>
2022-03-24 19:43:59 -0500

    commit tree 7c659fc469b4eada7b9a61c00294701f8fc8cbe4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+ updateTheDom()
+});
+function updateTheDom(){
+    document.getElementById('text')
+    .innerHTML = 'This is really cool!'
+
+}
